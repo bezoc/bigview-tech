c96646b1a2fdbbc7adfea25b0444ca34cda4f09d
Beatriz Zonaro <77759777+bezoc@users.noreply.github.com>
2025-07-01 13:32:38 -0300

    fix: mover arquivos de produção para a raiz

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/bigview-tech/", // <- isso resolve tela branca no GitHub Pages
+  base: "/bigview-tech/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
